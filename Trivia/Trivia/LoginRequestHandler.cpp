d862908c58d34b8a32375af344a90ac4b96ce2b4
yohai1309 <yohai1309@gmail.com>
2021-04-15 21:15:47 +0300

    Implementing empty function to complie

--- Trivia/Trivia/LoginRequestHandler.cpp
+++ Trivia/Trivia/LoginRequestHandler.cpp
@@ -1,1 +1,11 @@
 #include "LoginRequestHandler.h"
+
+bool LoginRequestHandler::isRequestRelevant(const RequestInfo& reqInfo)
+{
+    return false;
+}
+
+RequestResult LoginRequestHandler::handleRequest(const RequestInfo& reqInfo)
+{
+    return RequestResult();
+}
