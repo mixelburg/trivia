fa580e63761e3d3b3c9204a182be5ff1febca75f
yohai1309 <yohai1309@gmail.com>
2021-04-19 20:20:20 +0300

    Added cTor and Dtor

--- Trivia/Trivia/IRequestHandler.h
+++ Trivia/Trivia/IRequestHandler.h
@@ -25,6 +25,8 @@ struct RequestResult: public IRequest {
 class IRequestHandler
 {
 public:
+	IRequestHandler() = default;
+	virtual ~IRequestHandler() = default;
 	virtual bool isRequestRelevant(const RequestInfo& reqInfo) = 0;
 	virtual RequestResult handleRequest(const RequestInfo& reqInfo) = 0;
 };
