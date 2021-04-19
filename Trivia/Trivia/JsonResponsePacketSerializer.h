950d46e4028e5aa8dba1ee2ec2dbf6f80bf3b654
yohai1309 <yohai1309@gmail.com>
2021-04-19 19:17:56 +0300

    .

--- Trivia/Trivia/JsonResponsePacketSerializer.h
+++ Trivia/Trivia/JsonResponsePacketSerializer.h
@@ -19,8 +19,23 @@ struct ErrorResponse {
 
 class JsonResponsePacketSerializer
 {
+	/*
+	Function serialize the error response to buffer
+	input: struct of the error response
+	output: buffer with the error response
+	*/
 	static std::vector<unsigned char> serializeResponse(const ErrorResponse& errResponse);
+	/*
+	Function serialize the login response to buffer
+	input: strcut of the login response
+	output: buffer with the login response
+	*/
 	static std::vector<unsigned char> serializeResponse(LoginResponse);
+	/*
+	Function serialize the signup response to buffer
+	input: struct of the signup response
+	output: buffer with the signup response
+	*/
 	static std::vector<unsigned char> serializeResponse(SignupResponse);
 
 };
