b3d83b9e01eb0ca54363925cf524e5fcdfe24534
yohai1309 <yohai1309@gmail.com>
2021-04-19 19:17:15 +0300

    Added cpp file for the serizlizer function

--- Trivia/Trivia/JsonResponsePacketSerializer.cpp
+++ Trivia/Trivia/JsonResponsePacketSerializer.cpp
@@ -1,1 +1,10 @@
 #include "JsonResponsePacketSerializer.h"
+
+std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const ErrorResponse& errResponse)
+{
+    std::vector<unsigned char> buffer;
+    for (const auto ch : errResponse.message) {
+        buffer.push_back(ch);
+    }
+    return buffer;
+}
