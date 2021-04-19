5e6f7be98e6c9c6a7a06ad427ee9be7f371950fe
yohai1309 <yohai1309@gmail.com>
2021-04-19 17:57:49 +0300

    Added Comminicator cTor

--- Trivia/Trivia/Communicator.cpp
+++ Trivia/Trivia/Communicator.cpp
@@ -1,1 +1,16 @@
 #include "Communicator.h"
+#include <iostream>
+
+#define LISTEN_PORT 5050
+#define HELLO_LEN 5
+
+Communicator::Communicator()
+{
+	//setting the socket to communicate with the clients
+	m_serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	//throws an error if the socket is invalid
+	if (m_serverSocket == INVALID_SOCKET) {
+		throw std::exception(__FUNCTION__ " - socket");
+	}
+}
