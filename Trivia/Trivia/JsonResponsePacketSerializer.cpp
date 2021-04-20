28163ff905ebf03dd089f26641413c726b3a43d3
yohai1309 <yohai1309@gmail.com>
2021-04-20 15:14:37 +0300

    Updated serializer of statusResponse function

--- Trivia/Trivia/JsonResponsePacketSerializer.cpp
+++ Trivia/Trivia/JsonResponsePacketSerializer.cpp
@@ -2,6 +2,11 @@
 #include "JsonResponsePacketSerializer.h"
 #include "json.h"
 #include <iostream>
+#include "Helper.h"
+
+#define TO_NUMBER 48
+#define LEN_SIZE 4
+
 std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const ErrorResponse& errResponse)
 {
     std::vector<unsigned char> buffer;
@@ -17,10 +22,19 @@ std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const
 std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const IStatusResponse& statusResponse)
 {
     std::vector<unsigned char> buffer;
+    
+    //insert the message code
+    buffer.push_back(statusResponse.code + TO_NUMBER);
+    
+    //create the json object
     Json::Value root;
     root = "status:" + std::to_string(statusResponse.status);
-    const auto jsonData = root.asString();
-    for (const auto ch : jsonData) {
+    auto jsonData = root.asString();
+
+    //insert the data' size
+    std::string dataSize = Helper::getPaddedNumber(jsonData.length(), LEN_SIZE);
+
+    for (const auto ch : dataSize + jsonData) {
         buffer.push_back(ch);
     }
     return buffer;
