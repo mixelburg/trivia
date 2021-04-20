0b0bf53d16014aedcd6ea4643e3cb6bee030fe9e
yohai1309 <yohai1309@gmail.com>
2021-04-20 14:15:54 +0300

    Deleted serializeResponse function

--- Trivia/Trivia/JsonResponsePacketSerializer.cpp
+++ Trivia/Trivia/JsonResponsePacketSerializer.cpp
@@ -1,34 +1,21 @@
+#pragma warning(disable : 4996)
 #include "JsonResponsePacketSerializer.h"
 #include "json/json.h"
-
+#include "json/assertions.h"
+#include <iostream>
 std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const ErrorResponse& errResponse)
 {
     std::vector<unsigned char> buffer;
-    std::string dataInJson = "{\"message\": \"" + errResponse.message + "\"}"; // {"message": "ERROR"}
-    Json::Reader reader;
     Json::Value root;
-    if (reader.parse(dataInJson, root, false)) {
-        const std::string resultValue = root["result"].asString();
-        for (const auto ch : "{ \"message\": \"") {
-            buffer.push_back(ch);
-        }
-        for (const auto ch : resultValue) {
-            buffer.push_back(ch);
-        }
-        for (const auto ch : "\"}") {
-            buffer.push_back(ch);
-        }
-    }
-   
     return buffer;
 }
 
 std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const LoginResponse& loginResponse)
 {
     std::vector<unsigned char> buffer;
-    std::string dataInJson = "{\"message\": \"" + std::to_string(loginResponse.status) + "\"}"; // {"status": "1"}
-    for (const auto ch : dataInJson) {
-        buffer.push_back(ch);
-    }
+    //std::string dataInJson = "{\"message\": \"" + std::to_string(loginResponse.status) + "\"}"; // {"status": "1"}
+    //for (const auto ch : dataInJson) {
+    //    buffer.push_back(ch);
+    //}
     return buffer;
 }
