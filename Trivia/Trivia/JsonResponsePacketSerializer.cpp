b502486a50a5d9f6eb89a0930cd2a6a49cea8651
yohai1309 <yohai1309@gmail.com>
2021-04-20 14:34:38 +0300

    Added serializeResponse with loginRequest function

--- Trivia/Trivia/JsonResponsePacketSerializer.cpp
+++ Trivia/Trivia/JsonResponsePacketSerializer.cpp
@@ -17,9 +17,11 @@ std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const
 std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const LoginResponse& loginResponse)
 {
     std::vector<unsigned char> buffer;
-    //std::string dataInJson = "{\"message\": \"" + std::to_string(loginResponse.status) + "\"}"; // {"status": "1"}
-    //for (const auto ch : dataInJson) {
-    //    buffer.push_back(ch);
-    //}
+    Json::Value root;
+    root = "message:" + std::to_string(loginResponse.status);
+    const auto jsonData = root.asString();
+    for (const auto ch : jsonData) {
+        buffer.push_back(ch);
+    }
     return buffer;
 }
