7029c9c568c96f5db88b8750d2ded10a026b61f0
yohai1309 <yohai1309@gmail.com>
2021-04-25 16:19:32 +0300

    Added functions' headers and structs

--- Trivia/Trivia/JsonRequestPacketDeserializer.h
+++ Trivia/Trivia/JsonRequestPacketDeserializer.h
@@ -1,8 +1,31 @@
 #pragma once
 #include "IRequestHandler.h"
 #include "responseCodes.h"
+#include <vector>
+#include <string>
+
+struct LoginRequest {
+	std::string username;
+	std::string password;
+};
+
+struct SignupRequest : LoginRequest{
+	std::string email;
+};
 
 class JsonRequestPacketDeserializer
 {
+	/*
+	Function deserialize a client request into LoginRequest 
+	input: a buffer with the data of the request (json)
+	output: login request struct
+	*/
+	static LoginRequest deserializeLoginRequest(const std::vector<unsigned char>& buffer);
+	/*
+	Function deserialize a client request into SignupRequest
+	input: a buffer with the data of the request (json)
+	output: signup request struct
+	*/
+	static SignupRequest deserializeSignupRequest(const std::vector<unsigned char>& buffer);
 };
 
