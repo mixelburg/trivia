a362325fa7c82bb6dd38af56984b8c8e0263405e
yohai1309 <yohai1309@gmail.com>
2021-04-25 18:32:57 +0300

    Added fileds and functions' headers for LoginRequestHandler

--- Trivia/Trivia/LoginRequestHandler.h
+++ Trivia/Trivia/LoginRequestHandler.h
@@ -18,5 +18,22 @@ public:
 	output: struct contains the handle of the request 
 	*/
 	virtual RequestResult handleRequest(const RequestInfo& reqInfo) override;
+
+private:
+	//LoginManager& m_loginManager;
+	//RequestHandlerFactory& m_handlerFactory;
+
+	/*
+	Function performs a login
+	input: struct with the login information
+	output: struct with the result of the login
+	*/
+	RequestResult login(const RequestInfo& reqInfo);
+	/*
+	Function performs a signup
+	input: struct with the signup information
+	output: struct with the result of the signup
+	*/
+	RequestResult signup(const RequestInfo& reqInfo);
 };
 
