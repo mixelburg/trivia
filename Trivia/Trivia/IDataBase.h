b2130521be07147dc3207e59330c2672982c3ac0
yohai1309 <yohai1309@gmail.com>
2021-04-26 20:32:30 +0300

    Added virtual functions

--- Trivia/Trivia/IDataBase.h
+++ Trivia/Trivia/IDataBase.h
@@ -6,4 +6,7 @@ public:
 
 	virtual bool open() = 0;
 	virtual void close() = 0;
+	virtual auto isUser(const std::string& uname) const -> bool = 0;
+	virtual auto checkPassword(const std::string& uname, const std::string& pswd) const -> bool = 0;
+	virtual auto addUser(const std::string& uname, const std::string& pswd, const std::string& email) const -> void = 0;
 };
