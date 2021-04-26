e6eea475a100a05aebd7a1e1c428014e4229181a
yohai1309 <yohai1309@gmail.com>
2021-04-26 20:33:05 +0300

    Updated filed of db to be a pointer

--- Trivia/Trivia/SqliteDataBase.h
+++ Trivia/Trivia/SqliteDataBase.h
@@ -7,9 +7,9 @@ class SqliteDataBase : IDataBase {
 public:
 	virtual ~SqliteDataBase() = default;
 
-	auto isUser(const std::string& uname) const -> bool;
-	auto checkPassword(const std::string& uname, const std::string& pswd) const -> bool;
-	auto addUser(const std::string& uname, const std::string& pswd, const std::string& email) const -> void;
+	auto isUser(const std::string& uname) const -> bool override;
+	auto checkPassword(const std::string& uname, const std::string& pswd) const -> bool override;
+	auto addUser(const std::string& uname, const std::string& pswd, const std::string& email) const -> void override;
 
 	auto open() -> bool override;
 	void close() override;
