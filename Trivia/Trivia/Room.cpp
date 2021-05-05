a1dde9cc6a383324b2f4daec7e23e332ca8eaf7a
yohai1309 <yohai1309@gmail.com>
2021-05-05 17:30:26 +0300

    Added part of removeUser(still have notes cause == operator havnt implemeted yet)

--- Trivia/Trivia/Room.cpp
+++ Trivia/Trivia/Room.cpp
@@ -4,3 +4,12 @@ void Room::addUser(const LoggedUser& loggedUser)
 {
 	m_users.push_back(loggedUser);
 }
+
+void Room::removeUser(const LoggedUser& loggedUser)
+{
+	for (auto it = m_users.begin(); it != m_users.end(); ++it) {
+		/*if (loggedUser == *it) {
+			m_users.erase(it);
+		}*/
+	}
+}
