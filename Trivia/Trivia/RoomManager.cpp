c10ea3e6cbcc460b4637bdfd8389933d7f3f81d4
yohai1309 <yohai1309@gmail.com>
2021-05-05 17:55:41 +0300

    Added createRoom

--- Trivia/Trivia/RoomManager.cpp
+++ Trivia/Trivia/RoomManager.cpp
@@ -1,1 +1,8 @@
 #include "RoomManager.h"
+
+void RoomManager::createRoom(const LoggedUser& owner, const RoomData& roomData)
+{
+	Room newRoom(roomData);
+	newRoom.addUser(owner);
+	m_rooms.emplace(std::make_pair(roomData.id, newRoom));
+}
