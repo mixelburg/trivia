7549a82c5c6dbd57f9a9bcb273f0f79c2ec60c6f
yohai1309 <yohai1309@gmail.com>
2021-05-10 18:42:32 +0300

    Added structs for responses

--- Trivia/Trivia/JsonResponsePacketSerializer.h
+++ Trivia/Trivia/JsonResponsePacketSerializer.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include "Codes.h"
+#include "Room.h"
 
 struct IStatusResponse {
 public:
@@ -27,6 +28,34 @@ struct ErrorResponse {
 // strcuts for version 2 
 struct LogoutResponse : public IStatusResponse {
 	LogoutResponse(unsigned int status) : IStatusResponse(status, LOGOUT_CODE) {}
+};
+
+struct GetRoomsResponse : public IStatusResponse {
+	GetRoomsResponse(unsigned int status, std::vector<RoomData> rooms) : IStatusResponse(status, GET_ROOMS_CODE), rooms(rooms) {}
+	std::vector<RoomData> rooms;
+};
+
+struct GetPlayersInRoomResponse {
+	GetPlayersInRoomResponse(std::vector<std::string> players) : players(players) {}
+	std::vector<std::string> players;
+};
+
+struct GetHighScoreResponse : public IStatusResponse {
+	GetHighScoreResponse(unsigned int status, std::vector<std::string> statistics) : IStatusResponse(status, GET_HIGH_SCORE_CODE), statistics(statistics) {}
+	std::vector<std::string> statistics;
+};
+
+struct GetPersonalStatsResponse : public IStatusResponse {
+	GetPersonalStatsResponse(unsigned int status, std::vector<std::string> statistics) : IStatusResponse(status, GET_STATS_CODE), statistics(statistics) {}
+	std::vector<std::string> statistics;
+};
+
+struct JoinRoomResponse : public IStatusResponse {
+	JoinRoomResponse(unsigned int status) : IStatusResponse(status, JOIN_ROOM_CODE) {}
+};
+
+struct CreateRoomResponse : public IStatusResponse {
+	CreateRoomResponse(unsigned int status) : IStatusResponse(status, CREATE_ROOM_CODE) {}
 };
 
 class JsonResponsePacketSerializer
