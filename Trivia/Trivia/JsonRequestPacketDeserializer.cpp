0debbe5202bb2ca772f3311374823cb48c9328bc
yohai1309 <yohai1309@gmail.com>
2021-05-11 15:33:15 +0300

    Added deserialize of getPlayers request

--- Trivia/Trivia/JsonRequestPacketDeserializer.cpp
+++ Trivia/Trivia/JsonRequestPacketDeserializer.cpp
@@ -56,3 +56,33 @@ SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest(const std:
 	
 	return signupReq;
 }
+
+GetPlayersInRoomRequest JsonRequestPacketDeserializer::deserializeGetPlayersRequest(const std::vector<unsigned char>& buffer)
+{
+	GetPlayersInRoomRequest getPlayersReq;
+
+	//insreting the buffer into a string
+	std::string jsonStr;
+	for (const auto ch : buffer) {
+		jsonStr += ch;
+	}
+
+	//creating json objects to parse the data
+	Json::CharReaderBuilder builder;
+	Json::CharReader* reader = builder.newCharReader();
+
+	//creating json object to hold the data
+	Json::Value json;
+	std::string errors;
+
+	//parsing the std::string to a json object
+	if (!reader->parse(jsonStr.c_str(), jsonStr.c_str() + jsonStr.size(), &json, &errors)) {
+		delete reader;
+		throw std::exception("Failed to deserialize");
+	}
+	delete reader;
+
+	getPlayersReq.roomId = json.get("roomId", NULL).asInt();
+
+	return getPlayersReq;
+}
