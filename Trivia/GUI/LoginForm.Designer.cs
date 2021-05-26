e016e58817da66619f485c7ef8740274299fd40a
mixelburg <52622705+mixelburg@users.noreply.github.com>
2021-05-26 19:45:56 +0300

    login form styling added

---
+++ Trivia/GUI/LoginForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace GUI
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.loginButton = new System.Windows.Forms.Button();
+            this.textBoxUname = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.pictureBoxUname = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPass = new System.Windows.Forms.PictureBox();
+            this.AuthorizationLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUname)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.AccessibleName = "";
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginButton.Location = new System.Drawing.Point(161, 472);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(174, 57);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // textBoxUname
+            // 
+            this.textBoxUname.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxUname.Location = new System.Drawing.Point(123, 165);
+            this.textBoxUname.Name = "textBoxUname";
+            this.textBoxUname.Size = new System.Drawing.Size(298, 64);
+            this.textBoxUname.TabIndex = 1;
+            this.textBoxUname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPass.Location = new System.Drawing.Point(123, 266);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(298, 64);
+            this.textBoxPass.TabIndex = 2;
+            this.textBoxPass.UseSystemPasswordChar = true;
+            // 
+            // pictureBoxUname
+            // 
+            this.pictureBoxUname.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxUname.Image")));
+            this.pictureBoxUname.Location = new System.Drawing.Point(53, 165);
+            this.pictureBoxUname.Name = "pictureBoxUname";
+            this.pictureBoxUname.Size = new System.Drawing.Size(64, 64);
+            this.pictureBoxUname.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxUname.TabIndex = 3;
+            this.pictureBoxUname.TabStop = false;
+            // 
+            // pictureBoxPass
+            // 
+            this.pictureBoxPass.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxPass.Image")));
+            this.pictureBoxPass.Location = new System.Drawing.Point(53, 266);
+            this.pictureBoxPass.Name = "pictureBoxPass";
+            this.pictureBoxPass.Size = new System.Drawing.Size(64, 64);
+            this.pictureBoxPass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPass.TabIndex = 4;
+            this.pictureBoxPass.TabStop = false;
+            // 
+            // AuthorizationLabel
+            // 
+            this.AuthorizationLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AuthorizationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AuthorizationLabel.Location = new System.Drawing.Point(0, 0);
+            this.AuthorizationLabel.Name = "AuthorizationLabel";
+            this.AuthorizationLabel.Size = new System.Drawing.Size(506, 97);
+            this.AuthorizationLabel.TabIndex = 5;
+            this.AuthorizationLabel.Text = "Authorization";
+            this.AuthorizationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AuthorizationLabel.UseMnemonic = false;
+            this.AuthorizationLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(506, 609);
+            this.Controls.Add(this.AuthorizationLabel);
+            this.Controls.Add(this.pictureBoxPass);
+            this.Controls.Add(this.pictureBoxUname);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxUname);
+            this.Controls.Add(this.loginButton);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUname)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPass)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox textBoxUname;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.PictureBox pictureBoxUname;
+        private System.Windows.Forms.PictureBox pictureBoxPass;
+        private System.Windows.Forms.Label AuthorizationLabel;
+    }
+}
--- Trivia/GUI/LoginForm.cs
+++ Trivia/GUI/LoginForm.cs
@@ -10,14 +10,19 @@ using System.Windows.Forms;
 
 namespace GUI
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
