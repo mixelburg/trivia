dca83a225de6e5a4d325b5c788b84172c4ccf3d0
mixelburg <52622705+mixelburg@users.noreply.github.com>
2021-06-02 17:08:38 +0300

    more docs

--- Trivia/Trivia/IDataBase.h
+++ Trivia/Trivia/IDataBase.h
@@ -7,9 +7,34 @@ class IDataBase
 public:
 	virtual ~IDataBase() = default;
 
+	/**
+	 * @brief checks if user exists in database
+	 * @param uname name of user to check
+	 * @return true/false
+	*/
 	virtual bool isUser(const std::string& uname) const = 0;
+
+	/**
+	 * @brief checks if password is correct
+	 * @param uname name of user to check
+	 * @param pswd password to check
+	 * @return true/false
+	*/
 	virtual bool checkPassword(const std::string& uname, const std::string& pswd) const = 0;
+
+	/**
+	 * @brief adds user to the database
+	 * @param uname name of user to add
+	 * @param pswd password of user to add
+	 * @param email email of uer to add
+	*/
 	virtual void addUser(const std::string& uname, const std::string& pswd, const std::string& email) const = 0;
+
+	/**
+	 * @brief 
+	 * @param num 
+	 * @return 
+	*/
 	virtual std::list<Question> getQuestion(int num) const = 0;
 	virtual float getPlayerAverageAnswerTime(const std::string& uname) const = 0;
 	virtual int getNumOfCorrectAnswers(const std::string& uname) const = 0;
