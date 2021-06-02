b414f0730d44650dc9b4a6c203e78d12256427f5
yohai1309 <yohai1309@gmail.com>
2021-06-02 20:07:15 +0300

    Added deserialize for rooms msg

--- Trivia/GUI/Deserializer.cs
+++ Trivia/GUI/Deserializer.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace GUI
 {
@@ -7,21 +8,35 @@ namespace GUI
         public const int startIndex = 5;
 
         /// <summary>
-        ///     Function extract the status for the server response
+        ///     Function extract the status from the server response
         /// </summary>
         /// <param string with the server response="res"></param>
         /// <returns></returns>
         public static StatusStruct DeserializeStatusMsg(ref string res)
         {
             res = res.Substring(startIndex);
-            var serverRes = JsonConvert.DeserializeObject<StatusStruct>(res);
+            StatusStruct serverRes = JsonConvert.DeserializeObject<StatusStruct>(res);
 
             return serverRes;
         }
+        /// <summary>
+        ///     Function extract the rooms from the server response
+        /// </summary>
+        /// <param string with the server response="res"></param>
+        /// <returns></returns>
+        public static RoomsStruct DeserializeRoomsMsg(ref string res) {
+            res = res.Substring(startIndex);
+            RoomsStruct serverRes = JsonConvert.DeserializeObject<RoomsStruct>(res);
 
+            return serverRes;
+        }
         public struct StatusStruct
         {
             public string status;
+        }
+        public struct RoomsStruct {
+            public string status; 
+            public List<string> roomsNames;
         }
     }
 }
