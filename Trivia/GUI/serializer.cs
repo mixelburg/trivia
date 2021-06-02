6f198e48b19e6888e79f0f6d83e1742281553a25
mixelburg <52622705+mixelburg@users.noreply.github.com>
2021-06-02 12:31:50 +0300

    serializer improved

--- Trivia/GUI/serializer.cs
+++ Trivia/GUI/serializer.cs
@@ -19,6 +19,17 @@ namespace GUI
         public int questionCount;
         public int answerTimeout;
     }
+    public struct JoinRoomData
+    {
+        public int roomId;
+    }
+
+    public struct GetPlayersData
+    {
+        public int roomId;
+    }
+
+
     class Serializer
     {
         /// <summary>
@@ -30,33 +41,72 @@ namespace GUI
         struct codesClass {
             public static string loginCode = "1";
             public static string signupCode = "2";
-            public static string createRommCode = "A";
+            public static string createRoomCode = "A";
+            public static string joinRoomCode = "9";
+            public static string getPlayersCode = "6";
         }
+
+        public static string SerializeRequest<T>(ref T req, ref string code)
+        {
+            string request = code;
+            string jsonData = JsonConvert.SerializeObject(req, Formatting.Indented);
+            request += jsonData.Length.ToString().PadLeft(LengthSize, '0');
+            request += jsonData;
+            return request;
+        }
+
         public static string SerializeLoginRequest(ref LoginRequestData loginReq) {
-            string request = codesClass.loginCode;
-            string jsonData = JsonConvert.SerializeObject(loginReq, Formatting.Indented);
-            request += jsonData.Length.ToString().PadLeft(LengthSize, '0');          
-            request += jsonData;
-            return request;
+            // string request = codesClass.loginCode;
+            // string jsonData = JsonConvert.SerializeObject(loginReq, Formatting.Indented);
+            // request += jsonData.Length.ToString().PadLeft(LengthSize, '0');          
+            // request += jsonData;
+            // return request;
+
+            return SerializeRequest(ref loginReq, ref codesClass.loginCode);
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="signupReq"></param>
-        /// <returns></returns>
-        public static string SerializeSignupRequest(ref SignupRequestData signupReq) {
-            string request = codesClass.signupCode;
-            string jsonData = JsonConvert.SerializeObject(signupReq, Formatting.Indented);
-            request += jsonData.Length.ToString().PadLeft(LengthSize, '0');
-            request += jsonData;
-            return request;
+
+        public static string SerializeSignupRequest(ref SignupRequestData signupReq)
+        {
+            // string request = codesClass.signupCode;
+            // string jsonData = JsonConvert.SerializeObject(signupReq, Formatting.Indented);
+            // request += jsonData.Length.ToString().PadLeft(LengthSize, '0');
+            // request += jsonData;
+            // return request;
+
+            return SerializeRequest(ref signupReq, ref codesClass.loginCode);
         }
+
         public static string SerializeCreateRoomRequest(ref CreateRoomData createRoomReq) {
-            string request = codesClass.createRommCode;
-            string jsonData = JsonConvert.SerializeObject(createRoomReq, Formatting.Indented);
-            request += jsonData.Length.ToString().PadLeft(LengthSize, '0');
-            request += jsonData;
-            return request;
+            // string request = codesClass.createRoomCode;
+            // string jsonData = JsonConvert.SerializeObject(createRoomReq, Formatting.Indented);
+            // request += jsonData.Length.ToString().PadLeft(LengthSize, '0');
+            // request += jsonData;
+            // return request;
+
+            return SerializeRequest(ref createRoomReq, ref codesClass.loginCode);
         }
+
+        public static string SerializeJoinRoomRequest(ref JoinRoomData joinRoomData)
+        {
+            // string request = codesClass.joinRoomCode;
+            // string jsonData = JsonConvert.SerializeObject(joinRoomData, Formatting.Indented);
+            // request += jsonData.Length.ToString().PadLeft(LengthSize, '0');
+            // request += jsonData;
+            // return request;
+
+            return SerializeRequest(ref joinRoomData, ref codesClass.loginCode);
+        }
+
+        public static string SerializeGetPlayersRequest(ref GetPlayersData getPlayersData)
+        {
+            // string request = codesClass.getPlayersCode;
+            // string jsonData = JsonConvert.SerializeObject(getPlayersData, Formatting.Indented);
+            // request += jsonData.Length.ToString().PadLeft(LengthSize, '0');
+            // request += jsonData;
+            // return request;
+
+            return SerializeRequest(ref getPlayersData, ref codesClass.loginCode);
+        }
+        
     }
 }
